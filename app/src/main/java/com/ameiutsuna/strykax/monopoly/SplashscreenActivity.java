55eb4c5c6ba703ce3d14e17256ee09460a4b4ea2
strykax <mondeofchaos@gmail.com>
2017-04-18 18:07:38 +0200

    Create Base Project

---
+++ app/src/main/java/com/ameiutsuna/strykax/monopoly/SplashscreenActivity.java
@@ -0,0 +1,14 @@
+package com.ameiutsuna.strykax.monopoly;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashscreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+    }
+}
